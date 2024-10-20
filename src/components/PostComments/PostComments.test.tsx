50514efeab4741249ece614422b148e3cacd1cc5
Vitor Brito <joaovitor16611661@gmail.com>
2024-10-20 14:30:49 -0300

    inserção de teste para comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,31 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    test('Deve permitir comentar duas vezes', () => {
+        render(<PostComment/>);
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Olá, meu nome é João',
+            },
+        });
+        fireEvent.click(screen.getByTestId('btn-comentar'));
+        expect(screen.getByText('Olá, meu nome é João')).toBeInTheDocument();
+        
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Muito prazer',
+            },
+        });
+        fireEvent.click(screen.getByTestId('btn-comentar'));
+        expect(screen.getByText('Muito prazer')).toBeInTheDocument();
+
+        expect(screen.getByText('Olá, meu nome é João')).toBeInTheDocument();
+        expect(screen.getByText('Muito prazer')).toBeInTheDocument();
     });
 });
